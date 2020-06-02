e0dbe04059f4e568b7889165a98c9695b147f06b
limingkang <1151475385@qq.com>
2020-06-02 15:19:41 +0800

    测试样例

---
+++ bdata/src/test/java/com/site/bdata/BdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.bdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
