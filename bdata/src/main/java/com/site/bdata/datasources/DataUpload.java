7172811c01866969e2c5b7d6cf8c67b77b05d76b
limingkang <1151475385@qq.com>
2020-06-04 18:25:04 +0800

    项目数据已经可以录入

--- bdata/src/main/java/com/site/bdata/datasources/DataUpload.java
+++ bdata/src/main/java/com/site/bdata/datasources/DataUpload.java
@@ -1,8 +1,10 @@
 package com.site.bdata.datasources;
 
+import com.site.bdata.entity.BVideoData;
 import com.site.bdata.entity.BVideoHistory;
 import com.site.bdata.entity.BVideoRank;
 
+import com.site.bdata.service.BVideoDataService;
 import com.site.bdata.service.BVideoHistoryService;
 import com.site.bdata.service.BVideoRankService;
 import lombok.extern.slf4j.Slf4j;
@@ -30,6 +32,8 @@ public class DataUpload {
     private BVideoRankService bVideoRankService;
     @Resource
     private BVideoHistoryService bVideoHistoryService;
+    @Resource
+    private BVideoDataService bVideoDataService;
 
     private static DataUpload dataUpload;
 
@@ -37,36 +41,43 @@ public class DataUpload {
     public void init() {
         dataUpload = this;
         dataUpload.bVideoRankService = this.bVideoRankService;
-        dataUpload.bVideoHistoryService=this.bVideoHistoryService;
+        dataUpload.bVideoHistoryService = this.bVideoHistoryService;
+        dataUpload.bVideoDataService=this.bVideoDataService;
         // 初使化时将已静态化的testService实例化
     }
 
 
     /**
      * 上传全站日版数据
-     *
-     * */
+     */
     @Async
     public void dailyRankDataUpLoadToSQL() {
         List<BVideoRank> bVideoRankList = bilibiliRank.bVideoRankArraylist(0);
-        List<BVideoHistory> bVideoHistoryList=bVideoRankList.stream().map(bVideoRank->{
-            BVideoHistory bVideoHistory=new BVideoHistory();
+        List<BVideoData> bVideoDataList = new ArrayList<>();
+        List<BVideoHistory> bVideoHistoryList = bVideoRankList.stream().map(bVideoRank -> {
+            BVideoHistory bVideoHistory = new BVideoHistory();
             bVideoHistory.setBvNumber(bVideoRank.getBvNumber());
             bVideoHistory.setBvTitle(bVideoRank.getBvTitle());
             bVideoHistory.setBvUp(bVideoRank.getBvUp());
             bVideoHistory.setBvUpuuid(bVideoRank.getBvUpuuid());
             return bVideoHistory;
         }).collect(Collectors.toList());
+        for (BVideoHistory bVideoHistory : bVideoHistoryList) {
+            BVideoData bVideoData = bilibiliVideo.BVdata(bVideoHistory);
+            bVideoDataList.add(bVideoData);
+        }
+        boolean bVideoDataListflag = dataUpload.bVideoDataService.saveBatch(bVideoDataList);
+
+
+
         boolean bVideoRankListflag = dataUpload.bVideoRankService.saveBatch(bVideoRankList);
         boolean bVideoHistoryListflag = dataUpload.bVideoHistoryService.saveOrUpdateBatch(bVideoHistoryList);
-        if (bVideoRankListflag&&bVideoHistoryListflag) {
+        if (bVideoRankListflag && bVideoHistoryListflag&&bVideoDataListflag) {
             log.info("===========日版数据保存成功=============");
         } else {
             log.error("===========日版数据保存失败=============");
         }
     }
-
-
 
 
 }
