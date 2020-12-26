30188cd337b99ae8017855dd008a3418c4623bc6
limingkang <1151475385@qq.com>
2020-12-26 15:44:14 +0800

    append:自动化部署测试

---
+++ process/src/main/java/com/site/process/test/test.java
@@ -0,0 +1,11 @@
+package com.site.process.test;
+
+/**
+ * @author lenyuqin
+ * @data 2020/12/26
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("nihaoi");
+    }
+}
---
+++ upload/src/main/java/com/site/upload/test/test.java
@@ -0,0 +1,11 @@
+package com.site.upload.test;
+
+/**
+ * @author lenyuqin
+ * @data 2020/12/26
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
